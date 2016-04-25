6b80f48e807c14a2bb2891c262c69b2a771d9c95
Steve Klabnik <steve@steveklabnik.com>
2016-04-25 11:38:55 -0400

    move unsafe_binding::util into ruby-sys

---
+++ src/util.rs
@@ -0,0 +1,7 @@
+use types::{Argc, c_char, Id, Value};
+
+extern "C" {
+    pub fn rb_const_get(klass: Value, id: Id) -> Value;
+    pub fn rb_funcallv(receiver: Value, method: Value, argc: Argc, argv: *const Value) -> Value;
+    pub fn rb_intern(name: *const c_char) -> Id;
+}

ffa2e11355d049d54dca63827e1df81f2970edbc
Dmitry Gritsay <unseductable@gmail.com>
2016-06-30 21:05:38 +0300

    Add vm::rb_block_proc()

--- src/vm.rs
+++ src/vm.rs
@@ -1,6 +1,7 @@
 use types::{c_char, Value};
 
 extern "C" {
-    pub fn ruby_init();
+    pub fn rb_block_proc() -> Value;
     pub fn rb_require(name: *const c_char) -> Value;
+    pub fn ruby_init();
 }

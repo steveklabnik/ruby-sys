280e039c2a90ca27d8056a98f921aef18019f3ef
Dmitry Gritsay <unseductable@gmail.com>
2016-07-10 23:38:51 +0300

    Add rb_hash_size() function

--- src/hash.rs
+++ src/hash.rs
@@ -5,4 +5,5 @@ extern "C" {
     pub fn rb_hash_aset(hash: Value, key: Value, value: Value) -> Value;
     pub fn rb_hash_foreach(hash: Value, callback: CallbackPtr, pass: CallbackMutPtr);
     pub fn rb_hash_new() -> Value;
+    pub fn rb_hash_size(hash: Value) -> Value;
 }

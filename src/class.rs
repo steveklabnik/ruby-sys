d0c94f24af086c04e58a5a392b271a5ed808e6cf
Dmitry Gritsay <unseductable@gmail.com>
2016-09-06 21:21:13 +0300

    Add rb_define_class_under() function

--- src/class.rs
+++ src/class.rs
@@ -3,6 +3,7 @@ use types::{Argc, c_char, CallbackPtr, Id, Value};
 extern "C" {
     pub fn rb_class_new_instance(argc: Argc, argv: *const Value, klass: Value) -> Value;
     pub fn rb_define_class(name: *const c_char, superclass: Value) -> Value;
+    pub fn rb_define_class_under(outer: Value, name: *const c_char, superclass: Value) -> Value;
     pub fn rb_define_method(klass: Value, name: *const c_char, callback: CallbackPtr, argc: Argc);
     pub fn rb_define_module(name: *const c_char) -> Value;
     pub fn rb_ivar_get(object: Value, name: Id) -> Value;

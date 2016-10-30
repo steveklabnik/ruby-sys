e13fd1856601b948e2a45e32ea8e93befb337b1d
Dmitry Gritsay <unseductable@gmail.com>
2016-10-30 12:48:25 +0200

    Fix order of class functions (alphabetical)

--- src/class.rs
+++ src/class.rs
@@ -11,13 +11,13 @@ extern "C" {
     pub fn rb_define_method(klass: Value, name: *const c_char, callback: CallbackPtr, argc: Argc);
     pub fn rb_define_module(name: *const c_char) -> Value;
     pub fn rb_define_module_under(outer: Value, name: *const c_char) -> Value;
-    pub fn rb_include_module(klass: Value, module: Value);
     pub fn rb_extend_object(object: Value, module: Value);
-    pub fn rb_prepend_module(klass: Value, module: Value);
+    pub fn rb_include_module(klass: Value, module: Value);
     pub fn rb_ivar_get(object: Value, name: Id) -> Value;
     pub fn rb_ivar_set(object: Value, name: Id, value: Value) -> Value;
     pub fn rb_mod_ancestors(module: Value) -> Value;
     pub fn rb_obj_class(object: Value) -> Value;
+    pub fn rb_prepend_module(klass: Value, module: Value);
     pub fn rb_respond_to(object: Value, id: Id) -> c_int;
     pub fn rb_singleton_class(object: Value) -> Value;
 

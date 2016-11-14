95e43e79b6b86bfd8840b3c8f635d71d8e6bef06
Dmitry Gritsay <unseductable@gmail.com>
2016-11-14 23:38:24 +0200

    Add GVL-related functions to thread module
     - Add `rb_thread_call_without_gvl`, `rb_thread_call_without_gvl2`,
       `rb_thread_call_with_gvl` to `thread` module to prepare
       for their deprecation in `vm` module

--- src/thread.rs
+++ src/thread.rs
@@ -1,6 +1,20 @@
-use types::{c_void, RawFd, Value};
+use types::{CallbackPtr, c_void, RawFd, Value};
 
 extern "C" {
+    pub fn rb_thread_call_without_gvl(func: CallbackPtr,
+                                      args: *const c_void,
+                                      unblock_func: CallbackPtr,
+                                      unblock_args: *const c_void)
+                                      -> *mut c_void;
+
+    pub fn rb_thread_call_without_gvl2(func: CallbackPtr,
+                                       args: *const c_void,
+                                       unblock_func: CallbackPtr,
+                                       unblock_args: *const c_void)
+                                       -> *mut c_void;
+
+    pub fn rb_thread_call_with_gvl(func: CallbackPtr, args: *const c_void) -> *mut c_void;
+
     pub fn rb_thread_create(function: extern "C" fn(*mut c_void) -> Value,
                             data: *mut c_void)
                             -> Value;

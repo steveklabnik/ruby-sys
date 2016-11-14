809381fab4f2ce9439455e443fbe13aab7302d7d
Dmitry Gritsay <unseductable@gmail.com>
2016-11-15 00:05:44 +0200

    Deprecate GVL-related functions in vm module
     - Deprecate `rb_thread_call_without_gvl`, `rb_thread_call_without_gvl2`
       `rb_thread_call_with_gvl` in favor of `thread` module

--- src/vm.rs
+++ src/vm.rs
@@ -6,16 +6,25 @@ extern "C" {
     pub fn rb_block_given_p() -> c_int;
     pub fn rb_raise(exception: Value, message: *const c_char);
     pub fn rb_require(name: *const c_char) -> Value;
+    pub fn rb_protect(func: CallbackPtr, args: *const c_void, state: *mut c_int) -> Value;
+
+    #[deprecated(since="0.2.18",
+        note="Use `thread::rb_thread_call_without_gvl()` instead")]
     pub fn rb_thread_call_without_gvl(func: CallbackPtr,
                                       args: *const c_void,
                                       unblock_func: CallbackPtr,
                                       unblock_args: *const c_void)
                                       -> *mut c_void;
+
+    #[deprecated(since="0.2.18",
+        note="Use `thread::rb_thread_call_without_gvl2()` instead")]
     pub fn rb_thread_call_without_gvl2(func: CallbackPtr,
                                        args: *const c_void,
                                        unblock_func: CallbackPtr,
                                        unblock_args: *const c_void)
                                        -> *mut c_void;
+
+    #[deprecated(since="0.2.18",
+        note="Use `thread::rb_thread_call_with_gvl()` instead")]
     pub fn rb_thread_call_with_gvl(func: CallbackPtr, args: *const c_void) -> *mut c_void;
-    pub fn rb_protect(func: CallbackPtr, args: *const c_void, state: *mut c_int) -> Value;
 }

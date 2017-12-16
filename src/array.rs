05b10d9217e4ae0bf404cb39f34cd8ee01137772
Dmitry Gritsay <unseductable@gmail.com>
2017-12-16 19:10:05 +0200

    Add array::rb_ary_new_from_values()

--- src/array.rs
+++ src/array.rs
@@ -10,6 +10,7 @@ extern "C" {
     pub fn rb_ary_entry(array: Value, offset: c_long) -> Value;
     pub fn rb_ary_join(array: Value, separator: Value) -> Value;
     pub fn rb_ary_new() -> Value;
+    pub fn rb_ary_new_from_values(count: c_long, elements: *const Value) -> Value;
     pub fn rb_ary_new_capa(capacity: c_long) -> Value;
     pub fn rb_ary_pop(array: Value) -> Value;
     pub fn rb_ary_push(array: Value, item: Value) -> Value;

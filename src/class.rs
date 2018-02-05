08c96179e46ce8a5fec25e1b4aeb5d9c63a2245e
Daniel P. Clark <6ftdan@gmail.com>
2018-02-05 14:59:11 -0500

    private method and module method added
    Mentioned in PR https://github.com/d-unseductable/ruru/issues/56

--- src/class.rs
+++ src/class.rs
@@ -10,7 +10,9 @@ extern "C" {
     pub fn rb_define_const(klass: Value, name: *const c_char, value: Value);
     pub fn rb_define_method(klass: Value, name: *const c_char, callback: CallbackPtr, argc: Argc);
     pub fn rb_define_module(name: *const c_char) -> Value;
+    pub fn rb_define_module_function(klass: Value, name: *const c_char, callback: CallbackPtr, argc: Argc);
     pub fn rb_define_module_under(outer: Value, name: *const c_char) -> Value;
+    pub fn rb_define_private_method(klass: Value, name: *const c_char, callback: CallbackPtr, argc: Argc);
     pub fn rb_extend_object(object: Value, module: Value);
     pub fn rb_include_module(klass: Value, module: Value);
     pub fn rb_ivar_get(object: Value, name: Id) -> Value;

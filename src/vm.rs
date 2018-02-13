36d82cc69a93d001de22bc0a460ab88cd45f9c0e
Daniel P. Clark <6ftdan@gmail.com>
2018-02-13 00:09:07 -0500

    Thread specific error setter and getter

--- src/vm.rs
+++ src/vm.rs
@@ -4,10 +4,12 @@ extern "C" {
     pub fn ruby_init();
     pub fn rb_block_proc() -> Value;
     pub fn rb_block_given_p() -> c_int;
+    pub fn rb_errinfo() -> Value;
     pub fn rb_eval_string(string: *const c_char) -> Value;
     pub fn rb_eval_string_protect(string: *const c_char, state: *mut c_int) -> Value;
     pub fn rb_raise(exception: Value, message: *const c_char);
     pub fn rb_require(name: *const c_char) -> Value;
+    pub fn rb_set_errinfo(err: Value);
     pub fn rb_protect(func: CallbackPtr, args: *const c_void, state: *mut c_int) -> Value;
 
     #[deprecated(since="0.2.18",

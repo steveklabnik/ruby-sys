f5e40f47019576c39d74919cc5b36c5f2be07eb5
Daniel P. Clark <6ftdan@gmail.com>
2018-03-02 17:33:59 -0500

    raise from existing exception instance

--- src/vm.rs
+++ src/vm.rs
@@ -7,6 +7,7 @@ extern "C" {
     pub fn rb_errinfo() -> Value;
     pub fn rb_eval_string(string: *const c_char) -> Value;
     pub fn rb_eval_string_protect(string: *const c_char, state: *mut c_int) -> Value;
+    pub fn rb_exc_raise(exception: Value);
     pub fn rb_raise(exception: Value, message: *const c_char);
     pub fn rb_require(name: *const c_char) -> Value;
     pub fn rb_set_errinfo(err: Value);

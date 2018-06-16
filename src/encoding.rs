93f675366d71754e3c484f5a6d29600b22fe94c3
Daniel P. Clark <6ftdan@gmail.com>
2018-06-16 15:56:02 -0400

    add filesystem encoding lookup

--- src/encoding.rs
+++ src/encoding.rs
@@ -5,6 +5,7 @@ extern "C" {
     pub fn rb_enc_find_index(name: *const c_char ) -> c_int;
     pub fn rb_enc_get_index(obj: Value) -> c_int;
     pub fn rb_enc_set_index(obj: Value, encindex: c_int);
+    pub fn rb_filesystem_encindex() -> c_int;
     pub fn rb_locale_encindex() -> c_int;
     pub fn rb_to_encoding_index(obj: Value) -> c_int;
     pub fn rb_usascii_encindex() -> c_int;
